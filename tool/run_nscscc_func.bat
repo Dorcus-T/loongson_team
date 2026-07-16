331d779c48af7877336afd471a8cc724ab8c25cd
Dorcus-T <166224218@qq.com>
2026-07-16 22:51:30 +0800

    脚本升级

--- tool/run_nscscc_func.bat
+++ tool/run_nscscc_func.bat
@@ -5,9 +5,26 @@ setlocal
 set CHIPLAB_HOME=Z:\home\dorcus_t\chiplab
 set WSL_CHIPLAB=/home/dorcus_t/chiplab
 set TEST=nscscc_func
+set PC_TRACE=
+set DUMP_WAVE=
+set DIFFTEST=--disable-trace-comp
+set DIFF_FLAG=
+
+:parse_args
+if /i "%1"=="-v" set PC_TRACE=--show-pc-info
+if /i "%1"=="-w" set DUMP_WAVE=--dump-waveform 1
+if /i "%1"=="-d" (
+    set DIFFTEST=
+    set DIFF_FLAG=--diff
+)
+shift
+if not "%1"=="" goto parse_args
 
 echo ===============================================================
 echo   %TEST% Verilator Simulation
+if "%PC_TRACE%"=="--show-pc-info" echo   (verbose: per-cycle PC enabled)
+if "%DUMP_WAVE%"=="--dump-waveform 1" echo   (waveform: fst enabled)
+if "%DIFFTEST%"=="" echo   (difftest: NEMU compare enabled)
 echo ===============================================================
 echo.
 
@@ -22,7 +39,7 @@ echo    Build OK.
 echo.
 
 echo [2/4] Configuring simulation...
-wsl -d Ubuntu-22.04 -e bash -c "export CHIPLAB_HOME=%WSL_CHIPLAB% && cd %WSL_CHIPLAB%/sims/verilator/run_prog && rm -f config-software.mak && chmod +x configure.sh && bash ./configure.sh --run %TEST% --disable-trace-comp --output-uart-info"
+wsl -d Ubuntu-22.04 -e bash -c "export CHIPLAB_HOME=%WSL_CHIPLAB% && cd %WSL_CHIPLAB%/sims/verilator/run_prog && rm -f config-software.mak && chmod +x configure.sh && bash ./configure.sh --run %TEST% %DIFFTEST% --output-uart-info"
 if errorlevel 1 (
     echo ERROR: Configure failed!
     pause
@@ -42,10 +59,11 @@ echo    Compile OK.
 echo.
 
 echo [4/4] Running simulation...
-wsl -d Ubuntu-22.04 -e bash -c "export CHIPLAB_HOME=%WSL_CHIPLAB% && export PATH=%WSL_CHIPLAB%/toolchains/loongson-gnu-toolchain-8.3-x86_64-loongarch32r-linux-gnusf-v2.0/bin:$PATH && cd %WSL_CHIPLAB%/sims/verilator/run_prog && rm -rf ./obj/%TEST%_obj && mkdir -p ./obj/%TEST%_obj && cp -r %WSL_CHIPLAB%/software/examples/%TEST%/obj ./obj/%TEST%_obj/ && rm -rf ./tmp && mkdir -p ./tmp && cp ./obj/%TEST%_obj/obj/rom.vlog ./tmp/ && cat ./tmp/rom.vlog > ./tmp/ram.dat && ln -sf ../Makefile_run ./tmp/Makefile_run && cd ./tmp && timeout 600 ../output --dump-delay 0 --dump-waveform 0 --time-limit 0"
+wsl -d Ubuntu-22.04 -e bash -c "export CHIPLAB_HOME=%WSL_CHIPLAB% && export PATH=%WSL_CHIPLAB%/toolchains/loongson-gnu-toolchain-8.3-x86_64-loongarch32r-linux-gnusf-v2.0/bin:$PATH && cd %WSL_CHIPLAB%/sims/verilator/run_prog && rm -rf ./obj/%TEST%_obj && mkdir -p ./obj/%TEST%_obj && cp -r %WSL_CHIPLAB%/software/examples/%TEST%/obj ./obj/%TEST%_obj/ && rm -rf ./tmp && mkdir -p ./tmp && cp ./obj/%TEST%_obj/obj/rom.vlog ./tmp/ && cat ./tmp/rom.vlog > ./tmp/ram.dat && ln -sf ../Makefile_run ./tmp/Makefile_run && cd ./tmp && timeout 600 ../output --dump-delay 0 %DUMP_WAVE% --time-limit 0 %PC_TRACE% %DIFF_FLAG% && if [ -f logs/simu_trace.fst ]; then cp logs/simu_trace.fst ../../../../../IP/myCPU/tool/; fi"
 echo.
 echo ===============================================================
 echo   Simulation finished.
-echo   Logs: %CHIPLAB_HOME%\sims\verilator\run_prog\tmp\uart_output.txt
+if "%DUMP_WAVE%"=="--dump-waveform 1" echo   Waveform: tool\simu_trace.fst (gtkwave)
+echo   Logs  : %CHIPLAB_HOME%\sims\verilator\run_prog\tmp\uart_output.txt
 echo ===============================================================
 pause
--- tool/run_vivado_full.bat
+++ tool/run_vivado_full.bat
@@ -1,94 +1,46 @@
 @echo off
-
 setlocal EnableExtensions
-
-
-
 set "VIVADO=E:\AMDDesignTools\2025.2\Vivado\bin\vivado.bat"
 
-
-
 if not exist "%VIVADO%" (
-
     echo [ERROR] Vivado not found
-
     echo Path: %VIVADO%
-
     pause
-
     exit /b 1
-
 )
-
-
 
 set "TOOLDIR=Z:\home\dorcus_t\chiplab\IP\myCPU\tool"
 
-
-
 if not exist "%TOOLDIR%\" (
-
     echo [ERROR] Cannot find tool directory
-
     echo Path: %TOOLDIR%
-
     pause
-
     exit /b 1
-
 )
-
-
 
 pushd "%TOOLDIR%"
-
 if errorlevel 1 (
-
     echo [ERROR] Cannot pushd to: %TOOLDIR%
-
     pause
-
     exit /b 1
-
 )
 
-
-
 set "LOGDIR=%TOOLDIR%\runs"
-
 if not exist "%LOGDIR%" mkdir "%LOGDIR%"
 
-
-
 echo ============================================================
-
 echo Vivado Full Project Timing Analysis
-
 echo Project: loongson.xpr
-
 echo ============================================================
-
 echo.
-
-
 
 call "%VIVADO%" -mode batch -source vivado_full.tcl -notrace -log "%LOGDIR%/vivado_full.log" -journal "%LOGDIR%/vivado_full.jou"
 
-
-
 echo.
-
 echo ============================================================
-
 echo Latest full-project runs:
-
 dir /b /ad runs\full_* 2>nul
-
 echo ============================================================
-
-
 
 popd
-
 pause
-

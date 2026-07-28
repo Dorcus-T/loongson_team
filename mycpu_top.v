6be03affcc32876d7f4ff623242961bd8f9c41bf
hjy643 <2676639284@qq.com>
2026-07-28 10:55:36 +0800

    cacop小问题

--- mycpu_top.v
+++ mycpu_top.v
@@ -813,7 +813,7 @@ module core_top (
         .cacop_en      (icache_cacop_en),
         .cacop_code    (cacop_code),
         .cacop_va      (cacop_va),
-        .mmu_cacop_tag (if_tag),
+        .mmu_cacop_tag (ex_tag),
         .cacop_rdy     (icache_cacop_rdy),
         // AXI 接口
         .rd_req        (icache_rd_req),
